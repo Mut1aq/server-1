0da681f9a8bc0a9daf8994cbc7a203e5914540d3
Mutlaq <76498218+MutlaqFaharan@users.noreply.github.com>
2022-07-26 16:23:13 +0300

    cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 	);
 	res.setHeader(
 		"Access-Control-Allow-Headers",
-		"Content-Type, Authorization"
+		"Content-Type, Authorization, X-Requested-With"
 	);
 	next();
 });
